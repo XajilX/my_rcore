12d853142f316c6b03c3e405fcffeec4e3c2915f
ajil <844902240@qq.com>
2023-10-07 14:31:03 +0800

    remove target

--- os/src/logging.rs
+++ os/src/logging.rs
@@ -3,5 +3,8 @@ use log::{self, Level, LevelFilter, Log, Metadata, Record};
 struct SimLogger;
 
 impl Log for SimLogger {
-    fn enabled
+    fn enabled(&self, _metadata: &Metadata) -> bool { true }
+    fn log(&self, record: &Record) {
+        let color = match record.level()
+    }
 }
